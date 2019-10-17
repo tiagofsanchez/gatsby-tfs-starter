f7013f01feb72f4ff633a03e47c3069ca3e48a10
Tiago Sanchez <tiagofsanchez@gmail.com>
2019-10-17 17:46:35 +0800

    slug for MDX in, need to work on query

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,8 +5,22 @@ const _ = require("lodash");
 const moment = require("moment");
 const siteConfig = require("./data/SiteConfig");
 
+const { createFilePath } = require("gatsby-source-filesystem")
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
+  if (node.internal.type === "Mdx") {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      // Name of the field you are adding
+      name: "slug",
+      // Individual MDX node
+      node,
+      // Generated value based on filepath with "blog" prefix. you
+      // don't need a separating "/" before the value because
+      // createFilePath returns a path with the leading "/".
+      value: `/blog${value}`,
+    })}
   let slug;
   if (node.internal.type === "MarkdownRemark") {
     const fileNode = getNode(node.parent);
