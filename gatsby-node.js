e2be4c6dc0711d234759cade863d18e95430996c
Tiago Sanchez <tiagofsanchez@gmail.com>
2019-10-18 12:06:04 +0800

    slugs for mdx are now correct, need to work on query to render them properly

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,23 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
   if (node.internal.type === "Mdx") {
     const value = createFilePath({ node, getNode })
-    createNodeField({
+
+    if(  Object.prototype.hasOwnProperty.call(node, "frontmatter") &&
+    Object.prototype.hasOwnProperty.call(node.frontmatter, "title")
+    ) { 
+      createNodeField({
+        // Name of the field you are adding
+        name: "slug",
+        // Individual MDX node
+        node,
+        // Generated value based on filepath with "blog" prefix. you
+        // don't need a separating "/" before the value because
+        // createFilePath returns a path with the leading "/".
+        value: `/${_.kebabCase(node.frontmatter.title)}`,
+      })
+
+    } else {
+      createNodeField({
       // Name of the field you are adding
       name: "slug",
       // Individual MDX node
@@ -20,7 +36,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       // don't need a separating "/" before the value because
       // createFilePath returns a path with the leading "/".
       value: `/blog${value}`,
-    })}
+    })}}
   let slug;
   if (node.internal.type === "MarkdownRemark") {
     const fileNode = getNode(node.parent);
