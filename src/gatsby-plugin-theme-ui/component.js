78eb3a31461b9eacdaaaac3c318fe6bbfa4aeaae
tiagofsanchez <tiagofsanchez@users.noreply.github.com>
2019-11-04 19:09:18 +0800

    just changing the components as well

--- src/gatsby-plugin-theme-ui/component.js
+++ src/gatsby-plugin-theme-ui/component.js
@@ -1,24 +1,6 @@
-/** @jsx jsx */
-import { jsx } from "theme-ui"
-import { preToCodeBlock } from "mdx-utils"
-import PrismCodeBlock from "@theme-ui/prism"
-
-const CodeBlock = preProps => {
-  const props = preToCodeBlock(preProps)
-
-  if (props) {
-    const { codeString, ...restProps } = props
-
-    return (
-      <div sx={{ mb: 2 }}>
-        <PrismCodeBlock {...restProps}>{codeString}</PrismCodeBlock>
-      </div>
-    )
-  } else {
-    return <pre {...preProps} />
-  }
-}
-
+// src/gatsby-plugin-theme-ui/components.js
+import Prism from '@theme-ui/prism'
 export default {
-  pre: CodeBlock,
-} 
+  pre: props => props.children,
+  code: Prism,
+}
