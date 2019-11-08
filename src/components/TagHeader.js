b5ba97aa92560a6c6fc8186902a63c1562beb0c0
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-11-08 22:57:06 +0800

    just fixing package.json

--- src/components/TagHeader.js
+++ src/components/TagHeader.js
@@ -1,31 +1,36 @@
-import React from 'react'
+import React from "react";
 /** @jsx jsx */
-import { Styled, jsx } from 'theme-ui'
+import { Styled, jsx } from "theme-ui";
 
+const TagHeader = props => {
+  const { tag } = props;
+  return (
+    <Styled
+      sx={{
+        display: `flex`,
+        textAlign: `center`,
+        alignItems: `baseline`,
+        borderBottom: `1px solid`
+      }}
+    >
+      <Styled.h2 sx={{ color: `primary`, fontSize: `10`, mr: 2 }}>
+        Your filtered for:
+      </Styled.h2>
+      <Styled.h3
+        sx={{
+          color: `text`,
+          mr: 1,
+          mb: 4,
+          boxShadow: `0px 1px 5px rgba(0, 0, 0, 0.5)`,
+          p: `5px`,
+          borderRadius: `4px`,
+          boxSizing: `content-box`
+        }}
+      >
+        {tag}
+      </Styled.h3>
+    </Styled>
+  );
+};
 
-const TagHeader = (props) => { 
-    
-    const { tag } = props
-    return (
-      <Styled sx={{ display: `flex`, textAlign: `center`, alignItems: `baseline`, borderBottom: `1px solid`, color: `primary`}}>
-        <Styled.h2 sx={{ color: `primary`, fontSize: `10`, mr: 2 }}>
-          Your filtered for:
-        </Styled.h2>
-        <Styled.h3
-          sx={{
-            color: `text`,
-            mr: 1,
-            mb: 4,
-            boxShadow: `0px 1px 5px rgba(0, 0, 0, 0.5)`,
-            p: `5px`,
-            borderRadius: `4px`,
-            boxSizing: `content-box`
-          }}
-        >
-          {tag}
-        </Styled.h3>
-      </Styled>
-    );
-}
-
-export default TagHeader
+export default TagHeader;
