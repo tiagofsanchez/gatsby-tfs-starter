95116076379bf8e9ec4a8d45ce4bcf51f7699c18
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-11-12 22:34:06 +0800

    improving the style from the mini avatar

--- src/components/Avatar/SmallAvatar.js
+++ src/components/Avatar/SmallAvatar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import siteConfig from "../../../data/SiteConfig";
-import AvatarLinks from "./AvatarLinks"
+import AvatarLinks from "./AvatarLinks";
 
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui";
@@ -11,28 +11,28 @@ const SmallAvatar = props => {
 
   return (
     <>
-      <Styled.h3
+      <Styled.h2
         sx={{
           mt: 5,
           color: `primary`,
           mb: 0,
           borderTop: `1px solid`,
-          pt: "15px",
-          textAlign:`center`
+          pt: "15px"
         }}
       >
         {siteTitle}
-      </Styled.h3>
+      </Styled.h2>
 
-      <div sx={{ display: `flex`, alignItems: `center`}}>
+      <div sx={{ display: `flex`, alignItems: `center`, mb: 2, mt: 2 }}>
         <img
           src={avatar.photo}
-          sx={{ width: 70, height: 70, borderRadius: 999, mr: `15px`}}
+          sx={{ width: 70, height: 70, borderRadius: 999, mr: `15px` }}
         />
         <Styled.p>{avatar.description}</Styled.p>
       </div>
-      <AvatarLinks sx={{textAlign:`left`}} size={"small"} />
-      
+      <div sx={{ textAlign: `left` }}>
+        <AvatarLinks sx={{ display: `inline-block` }} size={"small"} />
+      </div>
     </>
   );
 };
